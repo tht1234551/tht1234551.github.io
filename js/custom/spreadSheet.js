2dbc4bec72cf21d22fcc9126d8f7268e7e1aaa9e
Jung <tht1234551@gmail.com>
2021-05-31 00:16:29 +0900

    spreadSheet

--- js/custom/spreadSheet.js
+++ js/custom/spreadSheet.js
@@ -1,15 +1,15 @@
 console.log("exel test module...");
 
 
-let url = "https://script.google.com/macros/s/AKfycbyZr0LKevcZJcpIcbFpqijBZcxEgAMruH5L6LvV_eRpCmFrsCzZZjjtn716zWONr3mgNg/exec";
+let url = "https://script.google.com/macros/s/AKfycbwO-VrOz_JxoDsxv2aVIfIvzT10K4Ls4ZeCH2kK9n9Jq03NHCIjwxq_cjwDsvD3pKuDSg/exec";
 
 function write(){
     $.ajax({
         url: url,
         data: {A:"a", B:"f"},
         type: "POST"
-    }).done(()=>{
-        console.log("success");
+    }).done((result)=>{
+        console.log(result);
     }).fail(()=>{
         console.log("fail");
     });
